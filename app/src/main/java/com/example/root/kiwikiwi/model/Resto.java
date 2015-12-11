215f8926f5be70ebcc74719dd795af42bee93c47
Nicolas Nadisic <nicolas.nadisic@gmail.com>
2015-12-11 10:29:04 +0100

    Ajout du Modele de base

---
+++ app/src/main/java/com/example/root/kiwikiwi/model/Resto.java
@@ -0,0 +1,82 @@
+package com.example.root.kiwikiwi.model;
+
+import java.util.List;
+
+/**
+ * Created by root on 11/12/15.
+ */
+public class Resto {
+    String nom;
+    String photo; // Chemin vers l'image, en res ou en url
+    String ouvert; // Ouvert ou Fermé, juste pour toString
+    String adresse;
+    String moyenPaiement;
+    Integer niveauTarif; //Entre 1 et 3
+    String tempsAttente;
+    String horaires;
+    Categorie categorie; // Resto U, Resto INSA, ...
+    String menuDuJour;
+    List<Avis> avis;
+
+
+    public Resto(String nom, String photo, String ouvert, String adresse, String moyenPaiement,
+                 Integer niveauTarif, String tempsAttente, String horaires, Categorie categorie,
+                 String menuDuJour, List<Avis> avis) {
+        this.nom = nom;
+        this.photo = photo;
+        this.ouvert = ouvert;
+        this.adresse = adresse;
+        this.moyenPaiement = moyenPaiement;
+        this.niveauTarif = niveauTarif;
+        this.tempsAttente = tempsAttente;
+        this.horaires = horaires;
+        this.categorie = categorie;
+        this.menuDuJour = menuDuJour;
+        this.avis = avis;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public String getOuvert() {
+        return ouvert;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public String getMoyenPaiement() {
+        return moyenPaiement;
+    }
+
+    public Integer getNiveauTarif() {
+        return niveauTarif;
+    }
+
+    public String getTempsAttente() {
+        return tempsAttente;
+    }
+
+    public String getHoraires() {
+        return horaires;
+    }
+
+    public Categorie getCategorie() {
+        return categorie;
+    }
+
+    public String getMenuDuJour() {
+        return menuDuJour;
+    }
+
+    public List<Avis> getAvis() {
+        return avis;
+    }
+}
